b534c4812968bcb43ef06d0f867538ea9db6a5ca
devmachiine <46693400+devmachiine@users.noreply.github.com>
2020-07-30 17:38:35 +0000

    Check single arg set in _ property

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const parse_arg = (arg) => {
         // single flag OR long value
     } else if (arg.startsWith('-')) {
         // get bool flags
+    } else {
+        '_'
     }
     return 0
 }
@@ -18,5 +20,9 @@ module.exports = (args = []) => {
         throw Error('__proto__ not allowed as an argument to prevent prototype pollution.')
     }
 
-    return {}
+    if (args.length == 2) {
+        throw 'todo use unbound count instead'
+    }
+
+    return args.length ? { '_': args[0] } : {}
 }
--- tests/cli.js
+++ tests/cli.js
@@ -2,19 +2,37 @@ module.exports = async ({ test, assert, affirm, alike }) => {
 
     const cli = require('../index')
 
+    const throws = (f) => {
+        const not_thrown = {}
+        try {
+            f()
+            throw not_thrown
+        } catch (err) {
+            if (err === not_thrown)
+                throw Error("didn't throw error as expected")
+            else return err
+        }
+    }
+
+    const test_err = (description, f, maybe_check = i => i) => test(description, () => {
+        const err = throws(f)
+        maybe_check(err)
+    })
+
     return [
-        test("doesn't allow prototype pollution", () => {
-            const not_thrown = {}
-            try {
-                cli([, , '__proto__'])
-                throw not_thrown
-            } catch (err) {
-                if (err === not_thrown)
-                    throw Error("didn't throw error as expected")
-            }
+        test_err("doesn't allow prototype pollution", () => {
+            cli([, , '__proto__'])
+        })
+        , test_err("multiple unflagged args throws error", () => {
+            cli(['one', 'two'])
         })
         , test("no args return empty object", () => {
             alike({}, cli())
         })
+        , test("single unflagged arg set in _ property", () => {
+            const single = cli(['there'])
+            alike({ '_': 'there' }, single)
+        })
+        
     ]
 }
