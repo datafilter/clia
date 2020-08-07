01dc2eebc1e3cad11b9307abadf44572f84cb2a1
devmachiine <46693400+devmachiine@users.noreply.github.com>
2020-08-07 15:52:05 +0000

    refactor to command line like input

--- tests/cli.js
+++ tests/cli.js
@@ -1,6 +1,8 @@
 module.exports = async ({ test, alike }) => {
 
-    const cli = require('../index')
+    const clia = require('../index')
+
+    const cli = (text) => text && clia(text.split(' ')) || clia(text)
 
     const throws = (f) => {
         const not_thrown = {}
@@ -21,81 +23,81 @@ module.exports = async ({ test, alike }) => {
 
     return [
         test_err("doesn't allow prototype pollution", () => {
-            cli(['__proto__'])
+            cli('__proto__')
         })
         , test("no args return empty object", () => {
             alike(cli(), { $$: [] })
         })
         , test("single unflagged arg set in $$ property", () => {
-            alike(cli(['there']), { $$: ['there'] })
-            alike(cli(['$']), { $$: ['$'] })
+            alike(cli('there'), { $$: ['there'] })
+            alike(cli('$'), { $$: ['$'] })
         })
         , test("multiple unflagged args set in $$ property", () => {
-            alike(cli(['one', 'two', 'three', '$four', '$$five', '$$$ix']),
+            alike(cli('one two three $four $$five $$$ix'),
                 { $$: ['one', 'two', 'three', '$four', '$$five', '$$$ix'] })
         })
         , test("letter flag can start with -$", () => {
-            alike(cli(['-$']), { $$: {}, $: true })
-            alike(cli(['-$ab']), { $$: {}, $: true, a: true, b: true })
+            alike(cli('-$'), { $$: {}, $: true })
+            alike(cli('-$ab'), { $$: {}, $: true, a: true, b: true })
         })
         , test("word flag can start with --$", () => {
-            alike(cli(['--$']), { $$: {}, $: true })
-            alike(cli(['--$a']), { $$: {}, $a: true })
+            alike(cli('--$'), { $$: {}, $: true })
+            alike(cli('--$a'), { $$: {}, $a: true })
         })
         , test("letter flag option sets boolean", () => {
-            alike(cli(['-v']), { $$: [], v: true })
-            alike(cli(['-vw']), { $$: [], v: true, w: true })
-            alike(cli(['-ab', '-b', '-c']), { $$: [], a: true, b: true, c: true })
-            alike(cli(['-a-_']), { $$: [], a: true, '-': true, '_': true })
-            alike(cli(['-v', 'lemon', '-s']), {
+            alike(cli('-v'), { $$: [], v: true })
+            alike(cli('-vw'), { $$: [], v: true, w: true })
+            alike(cli('-ab -b -c'), { $$: [], a: true, b: true, c: true })
+            alike(cli('-a-_'), { $$: [], a: true, '-': true, '_': true })
+            alike(cli('-v lemon -s'), {
                 $$: ['lemon'],
                 $v: ['lemon'],
                 v: true, s: true
             })
         })
         , test("single dash is argument", () => {
-            alike(cli(['-']), { $$: ['-'] })
+            alike(cli('-'), { $$: ['-'] })
         })
         , test("1st double dash is ignored ", () => {
-            alike(cli(['--']), { $$: [] })
+            alike(cli('--'), { $$: [] })
         })
         , test("arguments after -- are parsed verbatim", () => {
-            alike(cli(['--', 'a', '-b', '--c', '--d=e']), { $$: ['a', '-b', '--c', '--d=e'] })
-            alike(cli(['--', '$', '--']), { $$: ['$', '--'] })
-            alike(cli(['-a', 'b', '--', '$', '--']), { $$: ['b', '$', '--'], $a: ['b', '$', '--'], a: true })
+            alike(cli('-- a -b --c --d=e'), { $$: ['a', '-b', '--c', '--d=e'] })
+            alike(cli('-- $ --'), { $$: ['$', '--'] })
+            alike(cli('-a b -- $ --'), { $$: ['b', '$', '--'], $a: ['b', '$', '--'], a: true })
         })
         , test("word flag sets boolean", () => {
-            alike(cli(['--verbose']), { $$: [], verbose: true })
-            alike(cli(['--banana', '--apple', '--c', '-d']), {
+            alike(cli('--verbose'), { $$: [], verbose: true })
+            alike(cli('--banana --apple --c -d'), {
                 $$: [],
                 apple: true, banana: true, c: true, d: true
             })
         })
         , test_err("empty key with word flag throws error", () => {
-            cli(['--=val'])
+            cli('--=val')
         })
         , test_err("empty value with word flag throws error", () => {
-            cli(['--opt='])
+            cli('--opt=')
         })
         , test("word flag with equals sets string", () => {
-            alike(cli(['--opt=some']), { $$: [], $opt: 'some' })
+            alike(cli('--opt=some'), { $$: [], $opt: 'some' })
         })
         , test("post flag word arguments sets $value", () => {
-            alike(cli(['a', '-o', 'b', 'c']), {
+            alike(cli('a -o b c'), {
                 $$: ['a', 'b', 'c'],
                 $o: ['b', 'c'],
                 o: true
             })
         })
         , test("words and flags set different options: { $word:word, flag:true }", () => {
-            alike(cli(['--rover=some', '--rover']), { $$: [], $rover: 'some', rover: true })
-            alike(cli(['--rover', '--rover=mars']), { $$: [], $rover: 'mars', rover: true })
+            alike(cli('--rover=some --rover'), { $$: [], $rover: 'some', rover: true })
+            alike(cli('--rover --rover=mars'), { $$: [], $rover: 'mars', rover: true })
         })
         , test("long option overrides previous option", () => {
-            alike(cli(['--shape=square', '--shape=triangle']), { $$: [], $shape: 'triangle' })
+            alike(cli('--shape=square --shape=triangle'), { $$: [], $shape: 'triangle' })
         })
         , test("tag only created from single flags", () => {
-            alike(cli(['-ab', 'uno', '--dos', 'tres', 'cuatro']), {
+            alike(cli('-ab uno --dos tres cuatro'), {
                 $$: ['uno', 'tres', 'cuatro'],
                 $dos: ['tres', 'cuatro'],
                 a: true,
